69c7dfa08398cf2d9c1950a59db890b63d1cca21
HugoSanche <hbmclaren@gmail.com>
2025-05-03 20:59:51 -0600

    adding exaple testing spring bott

--- spri-boot-unit-test-main/2.00-starting-project/src/main/java/com/mycode/test/MvcTestingExampleApplication.java
+++ spri-boot-unit-test-main/2.00-starting-project/src/main/java/com/mycode/test/MvcTestingExampleApplication.java
@@ -1,6 +1,6 @@
-package com.luv2code.component;
+package com.mycode.test;
 
-import com.luv2code.component.models.CollegeStudent;
+import com.mycode.test.models.CollegeStudent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- spri-boot-unit-test-main/2.00-starting-project/src/main/java/com/mycode/test/models/StudentGrades.java
+++ spri-boot-unit-test-main/2.00-starting-project/src/main/java/com/mycode/test/models/StudentGrades.java
@@ -1,4 +1,4 @@
-package com.luv2code.component.models;
+package com.mycode.test.models;
 
 import org.springframework.stereotype.Component;
 
